a80ff5c01875b5ebb3c926b36f7239d0ea25e29f
elhajjaji.riad <riad@gmail.com>
2023-06-29 09:13:32 -0400

    ++

--- server.js
+++ server.js
@@ -14,12 +14,20 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 
 //Middlewares
+app.use(
+  cors({
+    origin: [
+      "https://stock-managment-frontend.vercel.app",
+      "https://front-end-stock.onrender.com",
+    ],
+  })
+);
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "*"
-    // "https://stock-managment-frontend.vercel.app"
-  );
+  // res.setHeader(
+  //   "Access-Control-Allow-Origin",
+  //   "*"
+  //   // "https://stock-managment-frontend.vercel.app"
+  // );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", "true");
