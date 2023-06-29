da1fbe93752aea8cc1452e527fa8929e981caf21
elhajjaji.riad <riad@gmail.com>
2023-06-29 08:32:33 -0400

    ++

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const path = require("path");
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://stock-managment-frontend.vercel.app"
+    "*"
+    // "https://stock-managment-frontend.vercel.app"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
